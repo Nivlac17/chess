48e9d634309b5815b1ef71b660e2c750749774a6
Calvin Merrell <calvinm7@byu.edu>
2025-03-04 15:26:49 -0700

    cleaned code with helper functions added to interface

--- shared/src/main/java/chess/PieceMovesCalculatorInterface.java
+++ shared/src/main/java/chess/PieceMovesCalculatorInterface.java
@@ -2,8 +2,6 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-
 import static chess.ChessGame.TeamColor.WHITE;
 
 public interface PieceMovesCalculatorInterface {
@@ -11,45 +9,68 @@ public interface PieceMovesCalculatorInterface {
 }
 
 class PieceMovesCalculator{
-    int row;
-    int col;
+    void lineMoves(int[][] directions, int row, int col, ChessBoard board, Collection<ChessMove> moves, ChessPosition myPosition){
+        for (int[] direction : directions){
+            int futureRow = row + direction[0];
+            int futureCol = col + direction[1];
+            isMovingLoop(futureRow, futureCol, board, direction,myPosition,moves);
+        }
+    }
+void isMovingLoop(int futureRow, int futureCol, ChessBoard board, int[] direction, ChessPosition myPosition,Collection<ChessMove> moves){
+    ChessPiece currentPiece = board.getPiece(myPosition);
+    boolean blocked = false;
+    while(!blocked){
+        if (!(futureRow > 8 || futureCol > 8 || futureRow <=0 || futureCol <=0 )){
+            ChessPosition futurePosition = new ChessPosition(futureRow,futureCol);
+            ChessPiece pieceInFuturePosition = board.getPiece(futurePosition);
+
+            if (pieceInFuturePosition != null) {
+                if (pieceInFuturePosition.getTeamColor() != currentPiece.getTeamColor()){
+                    moves.add(new ChessMove(myPosition, futurePosition, null));
+                }
+                blocked = true;
+            }else{
+                moves.add(new ChessMove(myPosition, futurePosition, null));
+                futureRow += direction[0];
+                futureCol += direction[1];
+            }
+        } else {blocked = true;
+        }
+    }
+}
+    void oneMove(int[][] directions, int row, int col, ChessBoard board, Collection<ChessMove> moves, ChessPosition myPosition){
+        ChessPiece currentPiece = board.getPiece(myPosition);
+        for (int[] direction : directions){
+            int futureRow = row + direction[0];
+            int futureCol = col + direction[1];
+            if (!(futureRow > 8 || futureCol > 8 || futureRow <=0 || futureCol <=0 )){
+                ChessPosition futurePosition = new ChessPosition(futureRow,futureCol);
+                ChessPiece pieceInFuturePosition = board.getPiece(futurePosition);
+
+                if (pieceInFuturePosition != null) {
+                    if (pieceInFuturePosition.getTeamColor() != currentPiece.getTeamColor()){
+                        moves.add(new ChessMove(myPosition, futurePosition, null));
+                    }
+                }else{
+                    moves.add(new ChessMove(myPosition, futurePosition, null));
+
+                }
+
+
+            }
+
+        }
+    }
 }
 
 
 class BishopMovesCalculator extends PieceMovesCalculator implements PieceMovesCalculatorInterface{
     public Collection<ChessMove> calculatePieceMoves(ChessBoard board, ChessPosition myPosition) {
-        this.row = myPosition.getRow();
-        this.col = myPosition.getColumn();
-        ChessPiece currentPiece = board.getPiece(myPosition);
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
         Collection<ChessMove> moves = new ArrayList<>();
-
-        int[][] Directions = {{1,1}, {-1,-1},{1,-1},{-1,1}};
-
-        for (int[] Direction : Directions){
-            int futureRow = this.row + Direction[0];
-            int futureCol = this.col + Direction[1];
-            boolean blocked = false;
-
-            while(!blocked){
-                if (!(futureRow > 8 || futureCol > 8 || futureRow <=0 || futureCol <=0 )){
-                    ChessPosition futurePosition = new ChessPosition(futureRow,futureCol);
-                    ChessPiece pieceInFuturePosition = board.getPiece(futurePosition);
-
-                    if (pieceInFuturePosition != null) {
-                        if (pieceInFuturePosition.getTeamColor() != currentPiece.getTeamColor()){
-                            moves.add(new ChessMove(myPosition, futurePosition, null));
-                        }
-                        blocked = true;
-                    }else{
-                        moves.add(new ChessMove(myPosition, futurePosition, null));
-                        futureRow += Direction[0];
-                        futureCol += Direction[1];
-                    }
-                } else {blocked = true;}
-
-            }
-
-        }
+        int[][] directions = {{1,1}, {-1,-1},{1,-1},{-1,1}};
+        lineMoves(directions,row,col,board, moves, myPosition);
         return moves;
     }
 }
@@ -57,33 +78,11 @@ class BishopMovesCalculator extends PieceMovesCalculator implements PieceMovesCa
 
 class KingMovesCalculator extends PieceMovesCalculator implements PieceMovesCalculatorInterface{
     public Collection<ChessMove> calculatePieceMoves(ChessBoard board, ChessPosition myPosition) {
-        this.row = myPosition.getRow();
-        this.col = myPosition.getColumn();
-        ChessPiece currentPiece = board.getPiece(myPosition);
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
         Collection<ChessMove> moves = new ArrayList<>();
-
-        int[][] Directions = {{1,1}, {-1,-1},{1,-1},{-1,1},     {0,1}, {1,0}, {0,-1}, {-1,0}};
-
-        for (int[] Direction : Directions){
-            int futureRow = this.row + Direction[0];
-            int futureCol = this.col + Direction[1];
-            if (!(futureRow > 8 || futureCol > 8 || futureRow <=0 || futureCol <=0 )){
-                ChessPosition futurePosition = new ChessPosition(futureRow,futureCol);
-                ChessPiece pieceInFuturePosition = board.getPiece(futurePosition);
-
-                if (pieceInFuturePosition != null) {
-                    if (pieceInFuturePosition.getTeamColor() != currentPiece.getTeamColor()){
-                        moves.add(new ChessMove(myPosition, futurePosition, null));
-                    }
-                }else{
-                    moves.add(new ChessMove(myPosition, futurePosition, null));
-
-                }
-
-
-            }
-
-        }
+        int[][] directions = {{1,1}, {-1,-1},{1,-1},{-1,1},     {0,1}, {1,0}, {0,-1}, {-1,0}};
+        oneMove(directions,row,col,board, moves, myPosition);
         return moves;
     }
 }
@@ -91,33 +90,11 @@ class KingMovesCalculator extends PieceMovesCalculator implements PieceMovesCalc
 
 class KnightMovesCalculator extends PieceMovesCalculator implements PieceMovesCalculatorInterface{
     public Collection<ChessMove> calculatePieceMoves(ChessBoard board, ChessPosition myPosition) {
-        this.row = myPosition.getRow();
-        this.col = myPosition.getColumn();
-        ChessPiece currentPiece = board.getPiece(myPosition);
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
         Collection<ChessMove> moves = new ArrayList<>();
-
-        int[][] Directions = {{1,2}, {-1,2},{1,-2},{-1,-2},     {2,1}, {-2,1}, {2,-1}, {-2,-1}};
-
-        for (int[] Direction : Directions){
-            int futureRow = this.row + Direction[0];
-            int futureCol = this.col + Direction[1];
-            if (!(futureRow > 8 || futureCol > 8 || futureRow <=0 || futureCol <=0 )){
-                ChessPosition futurePosition = new ChessPosition(futureRow,futureCol);
-                ChessPiece pieceInFuturePosition = board.getPiece(futurePosition);
-
-                if (pieceInFuturePosition != null) {
-                    if (pieceInFuturePosition.getTeamColor() != currentPiece.getTeamColor()){
-                        moves.add(new ChessMove(myPosition, futurePosition, null));
-                    }
-                }else{
-                    moves.add(new ChessMove(myPosition, futurePosition, null));
-
-                }
-
-
-            }
-
-        }
+        int[][] directions = {{1,2}, {-1,2},{1,-2},{-1,-2},     {2,1}, {-2,1}, {2,-1}, {-2,-1}};
+        oneMove(directions,row,col,board, moves, myPosition);
         return moves;
     }
 }
@@ -125,38 +102,11 @@ class KnightMovesCalculator extends PieceMovesCalculator implements PieceMovesCa
 
 class RookMovesCalculator extends PieceMovesCalculator implements PieceMovesCalculatorInterface{
     public Collection<ChessMove> calculatePieceMoves(ChessBoard board, ChessPosition myPosition) {
-        this.row = myPosition.getRow();
-        this.col = myPosition.getColumn();
-        ChessPiece currentPiece = board.getPiece(myPosition);
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
         Collection<ChessMove> moves = new ArrayList<>();
-
-        int[][] Directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-
-        for (int[] Direction : Directions){
-            int futureRow = this.row + Direction[0];
-            int futureCol = this.col + Direction[1];
-            boolean blocked = false;
-
-            while(!blocked){
-                if (!(futureRow > 8 || futureCol > 8 || futureRow <=0 || futureCol <=0 )){
-                    ChessPosition futurePosition = new ChessPosition(futureRow,futureCol);
-                    ChessPiece pieceInFuturePosition = board.getPiece(futurePosition);
-
-                    if (pieceInFuturePosition != null) {
-                        if (pieceInFuturePosition.getTeamColor() != currentPiece.getTeamColor()){
-                            moves.add(new ChessMove(myPosition, futurePosition, null));
-                        }
-                        blocked = true;
-                    }else{
-                        moves.add(new ChessMove(myPosition, futurePosition, null));
-                        futureRow += Direction[0];
-                        futureCol += Direction[1];
-                    }
-                } else {blocked = true;}
-
-            }
-
-        }
+        int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        lineMoves(directions,row,col,board, moves, myPosition);
         return moves;
     }
 }
@@ -164,38 +114,11 @@ class RookMovesCalculator extends PieceMovesCalculator implements PieceMovesCalc
 
 class QueenMovesCalculator extends PieceMovesCalculator implements PieceMovesCalculatorInterface{
     public Collection<ChessMove> calculatePieceMoves(ChessBoard board, ChessPosition myPosition) {
-        this.row = myPosition.getRow();
-        this.col = myPosition.getColumn();
-        ChessPiece currentPiece = board.getPiece(myPosition);
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
         Collection<ChessMove> moves = new ArrayList<>();
-
-        int[][] Directions = {{0,1}, {1,0}, {0,-1}, {-1,0},     {1,1}, {-1,-1},{1,-1},{-1,1}};
-
-        for (int[] Direction : Directions){
-            int futureRow = this.row + Direction[0];
-            int futureCol = this.col + Direction[1];
-            boolean blocked = false;
-
-            while(!blocked){
-                if (!(futureRow > 8 || futureCol > 8 || futureRow <=0 || futureCol <=0 )){
-                    ChessPosition futurePosition = new ChessPosition(futureRow,futureCol);
-                    ChessPiece pieceInFuturePosition = board.getPiece(futurePosition);
-
-                    if (pieceInFuturePosition != null) {
-                        if (pieceInFuturePosition.getTeamColor() != currentPiece.getTeamColor()){
-                            moves.add(new ChessMove(myPosition, futurePosition, null));
-                        }
-                        blocked = true;
-                    }else{
-                        moves.add(new ChessMove(myPosition, futurePosition, null));
-                        futureRow += Direction[0];
-                        futureCol += Direction[1];
-                    }
-                } else {blocked = true;}
-
-            }
-
-        }
+        int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0},     {1,1}, {-1,-1},{1,-1},{-1,1}};
+        lineMoves(directions,row,col,board, moves, myPosition);
         return moves;
     }
 }
@@ -203,8 +126,8 @@ class QueenMovesCalculator extends PieceMovesCalculator implements PieceMovesCal
 
 class PawnMovesCalculator extends PieceMovesCalculator implements PieceMovesCalculatorInterface{
     public Collection<ChessMove> calculatePieceMoves(ChessBoard board, ChessPosition myPosition) {
-        this.row = myPosition.getRow();
-        this.col = myPosition.getColumn();
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
         ChessPiece currentPiece = board.getPiece(myPosition);
         boolean isWhite = false;
         if(currentPiece.getTeamColor() == WHITE){
@@ -214,24 +137,24 @@ class PawnMovesCalculator extends PieceMovesCalculator implements PieceMovesCalc
         Collection<ChessMove> moves = new ArrayList<>();
         int futureRow;
         if(isWhite) {
-            futureRow = this.row + 1;
+            futureRow = row + 1;
         } else {
-            futureRow = this.row -1;
+            futureRow = row -1;
         }
-        int futureCol = this.col;
+        int futureCol = col;
         if (!(futureRow > 8 || futureCol > 8 || futureRow <=0 || futureCol <=0 )){
             ChessPosition futurePosition = new ChessPosition(futureRow,futureCol);
             ChessPiece pieceInFuturePosition = board.getPiece(futurePosition);
             if (pieceInFuturePosition == null) {
                 addPawnMoveOrPromotionToCollection(myPosition,futurePosition,moves);
-                if (this.row == 2 && isWhite) {
+                if (row == 2 && isWhite) {
                     futureRow = 4;
                     futurePosition = new ChessPosition(futureRow, futureCol);
                     pieceInFuturePosition = board.getPiece(futurePosition);
                     if (pieceInFuturePosition == null) {
                         moves.add(new ChessMove(myPosition, futurePosition, null));
                     }
-                } else if ((this.row == 7 && !isWhite)) {
+                } else if ((row == 7 && !isWhite)) {
                     futureRow = 5;
                     futurePosition = new ChessPosition(futureRow, futureCol);
                     pieceInFuturePosition = board.getPiece(futurePosition);
@@ -243,18 +166,18 @@ class PawnMovesCalculator extends PieceMovesCalculator implements PieceMovesCalc
         }
         //        Attack Right if White
         if(isWhite) {
-            futureRow = this.row + 1;
+            futureRow = row + 1;
         } else {
-            futureRow = this.row -1;
+            futureRow = row -1;
         }
-        attackDiagonal(futureRow, this.col + 1, board, moves, myPosition);
+        attackDiagonal(futureRow, col + 1, board, moves, myPosition);
         //        Attack Left if White
         if(isWhite) {
-            futureRow = this.row + 1;
+            futureRow = row + 1;
         } else {
-            futureRow = this.row -1;
+            futureRow = row -1;
         }
-        attackDiagonal(futureRow, this.col - 1, board, moves, myPosition);
+        attackDiagonal(futureRow, col - 1, board, moves, myPosition);
 
 
 
