40e43dd0d1c856e9609645a183c6e990486e1132
Calvin Merrell <calvinm7@byu.edu>
2025-03-06 18:11:42 -0700

    Phase 3 getting started completed. Ready for phase 3 implementation

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,17 @@
 import chess.*;
+import spark.Spark;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        int port = 8080;
+        server.Server server8080 = new server.Server();
+        server8080.run(port);
+        
+        System.out.println("♕ Chess Server Running ");
+
+        Spark.get("/myPage", (req, res) -> "sup");
     }
 }
