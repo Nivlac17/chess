22d8203721979f69f5ead92d6360edd1d8a42a39
Calvin Merrell <calvinm7@byu.edu>
2025-04-16 10:49:08 -0600

    passing tests, session closing unknown reason

---
+++ client/src/main/java/ui/websocket/LoadBoard.java
@@ -0,0 +1,14 @@
+package ui.websocket;
+
+import model.GameData;
+import websocket.messages.Notification;
+
+
+public class loadBoard {
+    GameData gameData;
+
+    void loadBoard(GameData gameData){
+
+    }
+
+}
--- client/src/main/java/ui/websocket/LoadGameHandler.java
+++
@@ -1,8 +0,0 @@
-package ui.websocket;
-
-import model.GameData;
-import websocket.messages.Notification;
-
-public interface LoadGameHandler {
-    void drawBoard(GameData gameData);
-}
