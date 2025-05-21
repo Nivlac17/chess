0f0676c561d293078549963a45c87b6ce511bcf0
Calvin Merrell <calvinm7@byu.edu>
2025-05-21 12:06:11 -0600

    server object created

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -4,5 +4,14 @@ public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+
+
+
+        int port = 8080;
+        server.Server server8080 = new server.Server();
+        server8080.run(port);
+        System.out.println("♕ Chess Server Running on port: " + port);
+        
     }
 }
