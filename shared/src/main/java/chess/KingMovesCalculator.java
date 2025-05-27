0b10a27c4050a8a19d79ac6b2cd35f06ee7981b4
Calvin Merrell <calvinm7@byu.edu>
2025-05-27 07:57:34 -0600

    cleane piece calculator king, rook

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -5,11 +5,9 @@ import java.util.Collection;
 
 public class KingMovesCalculator implements PieceMoveCalculatorInterface {
     public Collection<ChessMove> calculatedPieceMoves(ChessBoard board, ChessPosition myPosition) {
-        int row = myPosition.getRow();
-        int col = myPosition.getColumn();
         Collection<ChessMove> moves = new ArrayList<>();
         int[][] directions = {{1, 1}, {-1, 1}, {1, -1}, {-1, -1}, {1, 0}, {-1, 0}, {0, -1}, {0, 1}};
-        moveOneSquare(directions, row, col, board, myPosition, moves);
+        moveOneSquare(directions, myPosition.getRow(),  myPosition.getColumn(), board, myPosition, moves);
         return moves;
     }
 }
--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -5,11 +5,9 @@ import java.util.Collection;
 
 public class RookMovesCalculator implements PieceMoveCalculatorInterface{
     public Collection<ChessMove> calculatedPieceMoves(ChessBoard board, ChessPosition myPosition) {
-        int row = myPosition.getRow();
-        int col = myPosition.getColumn();
         Collection<ChessMove> moves = new ArrayList<>();
         int[][] directions = {{1,0},{-1,0},{0,-1},{0,1}};
-        loopMoves(moves, directions, row, col, myPosition, board);
+        loopMoves(moves, directions, myPosition.getRow(), myPosition.getColumn(), myPosition, board);
         return moves;
     }
 }
