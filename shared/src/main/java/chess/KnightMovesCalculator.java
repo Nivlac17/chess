aced64a8b38a94f5ccf3d337efe60d2170c70605
Calvin Merrell <calvinm7@byu.edu>
2025-05-27 07:55:52 -0600

    Cleaned Piece calculator knight

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -5,11 +5,9 @@ import java.util.Collection;
 
 public class KnightMovesCalculator implements PieceMoveCalculatorInterface {
     public Collection<ChessMove> calculatedPieceMoves(ChessBoard board, ChessPosition myPosition) {
-        int row = myPosition.getRow();
-        int col = myPosition.getColumn();
         Collection<ChessMove> moves = new ArrayList<>();
         int[][] directions = {{1, 2}, {-1, 2}, {1, -2}, {-1, -2},   {2, 1}, {-2, 1}, {2, -1}, {-2, -1}};
-        moveOneSquare(directions, row, col, board, myPosition, moves);
+        moveOneSquare(directions, myPosition.getRow(), myPosition.getColumn(), board, myPosition, moves);
         return moves;
     }
 }
